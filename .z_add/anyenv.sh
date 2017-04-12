9cf586cc38a547cbc1bb09885e6fed1930caa05c
kentrino <kentrino@h-kento.jp>
2017-04-12 14:22:51 +0900

    Fix anyenv, pyenv init.

--- .z_add/anyenv.sh
+++ .z_add/anyenv.sh
@@ -1,5 +1,5 @@
+export PATH=$HOME/.anyenv/bin:$PATH
 if type anyenv > /dev/null 2>&1; then
-  export PATH=$HOME/.anyenv/bin:$PATH
   eval "$(anyenv init -)"
   export USE_ANYENV=true
 fi
--- .z_add/python.sh
+++ .z_add/python.sh
@@ -1,6 +1,6 @@
+export PYENV_ROOT=$HOME/.pyenv
+export PATH=$PYENV_ROOT/bin:$PATH
 if type pyenv > /dev/null 2>&1 && ! $USE_ANYENV; then 
-  export PYENV_ROOT=$HOME/.pyenv
-  export PATH=$PYENV_ROOT/bin:$PATH
   eval "$(pyenv init -)"
 fi
 
