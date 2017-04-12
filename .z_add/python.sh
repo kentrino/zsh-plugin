d580030d01fbd279e715a93b509b0f7a02022e16
kentrino <kentrino@h-kento.jp>
2017-04-12 14:09:09 +0900

    fix(python) pyenv leak

--- .z_add/python.sh
+++ .z_add/python.sh
@@ -4,17 +4,18 @@ if type pyenv > /dev/null 2>&1 && ! $USE_ANYENV; then
   eval "$(pyenv init -)"
 fi
 
-if $USE_ANYENV; then
-  activate_path="$HOME/.anyenv/envs/pyenv/versions/`pyenv global`/bin"
-else
-  activate_path="$HOME/.pyenv/versions/`pyenv global`/bin"
+if type pyenv > /dev/null 2>&1; then
+  if $USE_ANYENV; then
+    activate_path="$HOME/.anyenv/envs/pyenv/versions/`pyenv global`/bin"
+  else
+    activate_path="$HOME/.pyenv/versions/`pyenv global`/bin"
+  fi
+
+  activate () {
+    source $activate_path/activate $1
+  }
+
+  deactivate () {
+    source $activate_path/deactivate
+  }
 fi
-
-# conda
-activate () {
-  source $activate_path/activate $1
-}
-
-deactivate () {
-  source $activate_path/deactivate
-}
