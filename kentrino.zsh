13bb438ada419eb1c611c683ce555afa36cde997
kentrino <kentrino@h-kento.jp>
2021-03-22 00:58:07 +0900

    fix script

--- kentrino.zsh
+++ kentrino.zsh
@@ -1,22 +1,21 @@
 export LANG=en_US.UTF-8
 
-pwd
-echo $0:A
+DIRECTORY=$(dirname $0:A)
 
-source 'src/anyenv.sh'
-source 'src/bazel.sh'
-source 'src/cd_ls.sh'
-source 'src/code.sh'
-source 'src/direnv.sh'
-source 'src/editor.sh'
-source 'src/flutter.sh'
-source 'src/git.sh'
-source 'src/go.sh'
-source 'src/haskell.sh'
-source 'src/java.sh'
-source 'src/jetbrains.sh'
-source 'src/peco.sh'
-source 'src/python.sh'
-source 'src/rust.sh'
-source 'src/uuidgen.sh'
-source 'src/z.sh'
+source "$DIRECTORY/src/anyenv.sh"
+source "$DIRECTORY/src/bazel.sh"
+source "$DIRECTORY/src/cd_ls.sh"
+source "$DIRECTORY/src/code.sh"
+source "$DIRECTORY/src/direnv.sh"
+source "$DIRECTORY/src/editor.sh"
+source "$DIRECTORY/src/flutter.sh"
+source "$DIRECTORY/src/git.sh"
+source "$DIRECTORY/src/go.sh"
+source "$DIRECTORY/src/haskell.sh"
+source "$DIRECTORY/src/java.sh"
+source "$DIRECTORY/src/jetbrains.sh"
+source "$DIRECTORY/src/peco.sh"
+source "$DIRECTORY/src/python.sh"
+source "$DIRECTORY/src/rust.sh"
+source "$DIRECTORY/src/uuidgen.sh"
+source "$DIRECTORY/src/z.sh"
