bdc1ef607c71bd5fa75b016834c9b2b2eae21ff6
kentrino <kentrino@h-kento.jp>
2021-03-22 04:13:58 +0900

    (docker.sh) change version string to master

--- src/docker.sh
+++ src/docker.sh
@@ -10,6 +10,6 @@ if [ ! -e "$HOME/.zsh/completion/_docker-compose" ]; then
     mkdir -p "$HOME/.zsh"
     mkdir -p "$HOME/.zsh/completion"
     curl \
-    -L "https://raw.githubusercontent.com/docker/compose/1.28.5/contrib/completion/zsh/_docker-compose" \
+    -L "https://raw.githubusercontent.com/docker/compose/master/contrib/completion/zsh/_docker-compose" \
     -o "$HOME/.zsh/completion/_docker-compose"
 fi
