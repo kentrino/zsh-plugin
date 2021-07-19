6a86752a74f29f058d86561cde7c4fe186c4007a
kentrino <kentrino@h-kento.jp>
2021-07-19 18:49:01 +0900

    fix pyenv setting

--- src/python.sh
+++ src/python.sh
@@ -1,4 +1,9 @@
-if type pyenv > /dev/null 2>&1; then
-    eval "$(pyenv init --path)"
+export PYENV_ROOT="$HOME/.anyenv/envs/pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+
+if command -v pyenv 1>/dev/null 2>&1; then
+    eval "$(pyenv init -)"
 fi
+
 export PATH="$HOME/.poetry/bin:$PATH"
