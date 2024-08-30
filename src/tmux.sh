82ac42d7146c94d56e31e85f52d8cf5710f4f6b3
kentrino <kentrino@h-kento.jp>
2024-08-31 04:08:14 +0900

    :fire: (tmux): Remove unused select-predefined-tmux-session function

--- src/tmux.sh
+++ src/tmux.sh
@@ -6,86 +6,74 @@
 
 # -u: exit the script on the use of undefined variable
 # -e: exit the script on error
-set -eu
+# set -eu
 
+# function select-predefined-tmux-session() {
+#     local green='\033[0;32m'
+#     local red='\033[0;31m'
+#     local nc='\033[0m'
+#     local detach='[Detatch]'
+#     local command_file=~/.tmux.yml
 
-# Explain how this function works.
-#
-# 1. Read command file from ~/.tmux.yml
-# 2. Get all sessions from tmux
-# 3. Get all keys from yml file
-# 4. Colorize lines by result of `grep -o`
-# 5. Add detach keyword if tmux is running
-# 6. Select session name with fzf
-# 7. If selected is detach, detach tmux
-# 8. If selected is existed, switch session
-# 9. If selected is not existed, create new session
-function select-predefined-tmux-session() {
-    local green='\033[0;32m'
-    local red='\033[0;31m'
-    local nc='\033[0m'
-    local detach='[Detatch]'
-    local command_file=~/.tmux.yml
+#     local candidate_names
+#     candidate_names=$(yq keys "$command_file" | sed -e 's/- //')
 
-    local candidate_names
-    candidate_names=$(yq keys "$command_file" | sed -e 's/- //')
+#     local current_sessions
+#     current_sessions=$(tmux list-sessions)
 
-    local current_sessions
-    current_sessions=$(tmux list-sessions)
+#     local candidates
+#     candidates=$()
 
-    local candidates
-    candidates=$()
+#     for name in $candidate_names; do
+#         local contains
+#         contains=$(echo "$current_sessions" | grep -o "$name" | wc -w)
+#         # number equal
+#         if [ "$contains" -ne 0 ]; then
+#             # if session is started
+#             # first $ enable to contain new line character
+#             candidates=$(printf "%s\n%s%s%s" "$candidates" "$green" "$name" "$nc")
+#         else
+#             # if session is not started
+#             candidates=$(printf "%s\n%s%s%s" "$candidates" "$red" "$name" "$nc")
+#         fi
+#     done
 
-    for name in $candidate_names; do
-        local contains
-        contains=$(echo "$current_sessions" | grep -o "$name" | wc -w)
-        # number equal
-        if [ "$contains" -ne 0 ]; then
-            # if session is started
-            # first $ enable to contain new line character
-            candidates=$(printf "%s\n%s%s%s" "$candidates" "$green" "$name" "$nc")
-        else
-            # if session is not started
-            candidates=$(printf "%s\n%s%s%s" "$candidates" "$red" "$name" "$nc")
-        fi
-    done
+#     if [ -n "${TMUX+x}" ]; then
+#         candidates=$(printf "%s\n%s" "$candidates" "$detach")
+#     fi
 
-    if [ -n "${TMUX+x}" ]; then
-        candidates=$(printf "%s\n%s" "$candidates" "$detach")
-    fi
+#     local selected
+#     selected=$(echo "$candidates" | awk 'NF' | fzf --ansi)
 
-    local selected
-    selected=$(echo "$candidates" | awk 'NF' | fzf --ansi)
-
-    if [ "$selected" = "" ]; then
-        # do nothing
-        exit 0
-    elif [ "$selected" = "$detach" ]; then
-        if [ -n "$TMUX" ]; then
-            tmux detach
-        fi
-    else
-        local current_directory
-        current_directory=$(yq ".$selected.cd" "$command_file")
-        local command
-        command=$(yq ".$selected.command" "$command_file")
-        local contains
-        contains=$(echo "$current_sessions" | grep -o "$selected" | wc -w)
-        if [ "$contains" -ne 0 ]; then
-            # if session is started
-            if [ -n "${TMUX+x}" ]; then
-                # in tmux session
-                tmux switch -t "$selected"
-            else
-                # outside tmux session
-                tmux attach-session -t "$selected"
-            fi
-        else
-            # if session is not started
-            tmux new -s "$selected" -c "$current_directory" "$command"
-        fi
-    fi
-}
+#     if [ "$selected" = "" ]; then
+#         # do nothing
+#         exit 0
+#     elif [ "$selected" = "$detach" ]; then
+#         if [ -n "$TMUX" ]; then
+#             tmux detach
+#         fi
+#     else
+#         local current_directory
+#         current_directory=$(yq ".$selected.cd" "$command_file")
+#         local command
+#         command=$(yq ".$selected.command" "$command_file")
+#         local contains
+#         contains=$(echo "$current_sessions" | grep -o "$selected" | wc -w)
+#         if [ "$contains" -ne 0 ]; then
+#             # if session is started
+#             if [ -n "${TMUX+x}" ]; then
+#                 # in tmux session
+#                 tmux switch -t "$selected"
+#             else
+#                 # outside tmux session
+#                 tmux attach-session -t "$selected"
+#             fi
+#         else
+#             # if session is not started
+#             tmux new -s "$selected" -c "$current_directory" "$command"
+#         fi
+#     fi
+# }
 
 function select-tmux-session() {
     if [ -n "$TMUX" ]; then
