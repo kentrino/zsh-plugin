a2225c38b326b17558ceb062f7cd58623224e7ae
kentrino <kentrino@h-kento.jp>
2026-05-19 09:45:41 +0900

    feat(zsh_history): add comprehensive history configuration with sharing and timestamps

--- src/zsh_history.sh
+++ src/zsh_history.sh
@@ -1,3 +1,28 @@
+export HISTFILE=${HOME}/.zsh_history
+# number of commands in memoery
+export HISTSIZE=1000000
+# number of commands in file
+export SAVEHIST=100000000
+
+# https://unix.stackexchange.com/questions/568907/why-do-i-lose-my-zsh-history
+setopt hist_save_no_dups
+
+# ref:
+#   https://qiita.com/ykit00/items/94393d4c55b0b659a267
+#   https://zenn.dev/rclab/articles/present_my_zshrc
+# record timestamp
+setopt extended_history
+
+# share history across tabs/terminals (incompatible with INC_APPEND_HISTORY_TIME)
+setopt SHARE_HISTORY
+
+# INC_APPEND_HISTORY_TIME: writes history with duration after command finishes,
+#   but overrides SHARE_HISTORY and disables cross-tab sharing
+# setopt INC_APPEND_HISTORY_TIME
+
+# Setting for per-directory-history
+HISTORY_START_WITH_GLOBAL=true
+
 zshaddhistory() {
   emulate -L zsh
 
