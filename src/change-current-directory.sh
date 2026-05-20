d79f6608816a9b3a40d57cadcebcf19eaa563651
kentrino <kentrino@h-kento.jp>
2026-05-20 12:04:18 +0900

    chore: add lint workflow and pin action versions

--- src/change-current-directory.sh
+++ src/change-current-directory.sh
@@ -2,10 +2,11 @@
 
 change-current-directory() {
     current_directory="$(pwd)/"
-    current_directory_escaped="$(echo $current_directory | sed 's/\//\\\//g')"
+    current_directory_escaped="$(echo "$current_directory" | sed 's/\//\\\//g')"
 
     selected=$(fd . "$current_directory" --max-depth 2 --type d | sed -e "s/$current_directory_escaped//" | fzf)
     if [ -n "$selected" ]; then
+        # shellcheck disable=SC2034 # BUFFER is read by zsh's line editor
         BUFFER="cd $current_directory$selected"
         zle redisplay
     fi
